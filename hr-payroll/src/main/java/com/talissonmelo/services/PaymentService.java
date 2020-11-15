4cfe47dda61b243d053421c6a0295768e64ea913
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-11-15 11:04:08 -0300

    RestTemplate

--- hr-payroll/src/main/java/com/talissonmelo/services/PaymentService.java
+++ hr-payroll/src/main/java/com/talissonmelo/services/PaymentService.java
@@ -1,13 +1,30 @@
 package com.talissonmelo.services;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
 import com.talissonmelo.entities.Payment;
+import com.talissonmelo.entities.Worker;
 
 @Service
 public class PaymentService {
 
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
+	@Autowired
+	private RestTemplate restTemplate;
+
 	public Payment getPayment(Long workerId, int days) {
-		return new Payment("Talisson", 15.00, days);
+		Map<String, String> uriVariables = new HashMap<>();
+		uriVariables.put("id", workerId.toString());
+
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, uriVariables);
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 }
