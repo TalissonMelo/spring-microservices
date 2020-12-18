fea0f12864bebe735afb1a168332e1c05101164f
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-12-18 19:28:54 -0300

    User, Role, many to many association

---
+++ hr-user/src/main/java/com/talissonmelo/entities/Role.java
@@ -0,0 +1,29 @@
+package com.talissonmelo.entities;
+
+import java.io.Serializable;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+@Getter
+@Entity
+@Table(name = "tb_role")
+public class Role implements Serializable{
+	private static final long serialVersionUID = 1L;
+	
+	@EqualsAndHashCode.Include
+	@Id
+	private Long id;
+	private String roleName;
+}
