e7d5be8f505529c1d3f4e35b481765f6eac0b127
TalissonMelo <talissonmelorodrigues@gmail.com>
2020-12-19 11:44:04 -0300

    Login e Token JWT

--- hr-oauth/src/main/java/com/talissonmelo/entities/User.java
+++ hr-oauth/src/main/java/com/talissonmelo/entities/User.java
@@ -1,8 +1,14 @@
 package com.talissonmelo.entities;
 
 import java.io.Serializable;
+import java.util.Collection;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
@@ -15,7 +21,7 @@ import lombok.Setter;
 @AllArgsConstructor
 @Setter
 @Getter
-public class User implements Serializable {
+public class User implements UserDetails, Serializable {
 	private static final long serialVersionUID = 1L;
 
 	private long id;
@@ -23,4 +29,36 @@ public class User implements Serializable {
 	private String email;
 	private String password;
 	Set<Role> roles = new HashSet<>();
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return roles.stream()
+				.map(role -> new SimpleGrantedAuthority(role.getRoleName()))
+				.collect(Collectors.toList());
+	}
+
+	@Override
+	public String getUsername() {
+		return email;
+	}
+
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
 }
--- hr-oauth/src/main/java/com/talissonmelo/services/UserService.java
+++ hr-oauth/src/main/java/com/talissonmelo/services/UserService.java
@@ -1,6 +1,9 @@
 package com.talissonmelo.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.talissonmelo.entities.User;
@@ -10,12 +13,24 @@ import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
 
 	@Autowired
 	private UserFeignClients feignClient;
 
 	public User findByEmail(String email) {
+		User user = feignClient.findByEmail(email).getBody();
+		if (user == null) {
+			log.info("E-mail not found : " + email);
+			throw new IllegalArgumentException("Email not found");
+		}
+
+		log.info("E-mail found : " + email);
+		return user;
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
 		User user = feignClient.findByEmail(email).getBody();
 		if (user == null) {
 			log.info("E-mail not found : " + email);
